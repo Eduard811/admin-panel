ae32d63a0ca24fcbe972b265cad8d9bd8f679b44
Eduard811 <eduard.albertyan1@mail.ru>
2021-04-06 16:12:18 +0300

    connect database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-const start = () => {
+const start = async () => {
     try {
+        await mongoose.connect('mongodb+srv://eduard:eduard@cluster0.fwlvw.mongodb.net/quins?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
         app.listen(PORT, () => console.log(`server started on port ${PORT}`))
     } catch (error) {
         console.log(error)
