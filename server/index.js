992c903bd54ee10b782311b72fd191d111657abe
Eduard811 <eduard.albertyan1@mail.ru>
2021-04-06 16:41:17 +0300

    create authRoute, authController

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const authRoute = require('./routes/authRoute')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+app.use(express.json())
+app.use('/auth', authRoute)
 
 const start = async () => {
     try {
