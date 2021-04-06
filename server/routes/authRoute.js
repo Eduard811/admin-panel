f54a34a23ca77883a502936097fa523833b6c78c
Eduard811 <eduard.albertyan1@mail.ru>
2021-04-06 21:27:58 +0300

    roleMiddleware

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,14 @@
 const Router = require('express')
 const router = new Router()
 const authController = require('../controllers/authController')
+const { check } = require('express-validator')
+const roleMiddleware = require('../middleware/roleMiddleware')
 
-router.post('/registration', authController.registration)
+router.post('/registration', [
+    check('username', 'Имя пользователя не может быть пустым').notEmpty(),
+    check('password', 'Слишком маленький или большой пароль').isLength({min: 4, max: 12})
+], authController.registration)
 router.post('/login', authController.login)
-router.get('/users', authController.getUsers)
+router.get('/users', roleMiddleware(['ADMIN']), authController.getUsers)
 
 module.exports = router 
